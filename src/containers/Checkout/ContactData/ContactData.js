1fca77490b5c1a15941af7c67816b06aee8f038a
quizzle1 <40326220+quizzle1@users.noreply.github.com>
2019-09-05 14:51:21 +0300

    dynamic inputs are added

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,14 +4,69 @@ import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component{
     state = {
-        name: '',
-        email: '',
-        address: {
-            street:'',
-            postalCode: ''
+      // inputChangedHandler = (event, formElementIdentifier) => {
+      //   const updatedOrderForm = {...this.state.orderForm};
+      //   const updatedFormElement = {...updatedOrderForm[formElementIdentifier]}
+  
+      //   updatedFormElement.value = event.target.value
+      //   updatedOrderForm[formElementIdentifier] = updatedFormElement
+      //   this.setState({orderForm: updatedOrderForm})
+      // }
+        orderForm: {                     
+          name: {
+            elementType: 'input',
+            elementConfig: {
+              type: 'text',
+              placeholder: 'Your Name'
+            },
+            value: ''
+          },
+          email: {
+            elementType: 'input',
+            elementConfig: {
+              type: 'email',
+              placeholder: 'Your Email'
+            },
+            value: ''
+          },
+          street: {
+            elementType: 'input',
+            elementConfig: {
+              type: 'text',
+              placeholder: 'Your Street'
+            },
+            value: ''
+          },
+          country: {
+            elementType: 'input',
+            elementConfig: {
+              type: 'text',
+              placeholder: 'Your Country'
+            },
+            value: ''
+          },
+          zipCode: {
+            elementType: 'input',
+            elementConfig: {
+              type: 'text',
+              placeholder: 'ZIP Code'
+            },
+            value: ''
+          },
+          deliveryMethod: {
+            elementType: 'select',
+            elementConfig: {
+              options: [
+                {value: 'fastest', displayValue: 'Fastest'},
+                {value: 'Cheapest', displayValue: 'Cheapest'}
+              ]
+            },
+            value: ''
+          }
         },
         loading: false
     }
@@ -19,20 +74,19 @@ class ContactData extends Component{
     orderHandler = (event) => {
         event.preventDefault();
 
+        const formData = {}
+        for (let formElementIdentifier in this.state.orderForm) {
+          formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+        }
+        console.log(formData);
+        
+
         this.setState( {loading: true} )
         const order = {
           ingredients: this.props.ingredients,
           price: this.props.price,
-          customer: {
-            name: 'Tolunay',
-            address: {
-              street: 'test street',
-              countr: 'Turkey'
-    
-            },
-            email: 'test@test.com'
-          },
-          deliveryMethod: 'fastest'
+          orderData: formData
+          
         }
         axios.post('/orders.json', order)
           .then(response => {
@@ -44,15 +98,38 @@ class ContactData extends Component{
           })
     }
 
+    inputChangedHandler = (event, formElementIdentifier) => {
+      const updatedOrderForm = {...this.state.orderForm};
+      const updatedFormElement = {...updatedOrderForm[formElementIdentifier]}
+
+      updatedFormElement.value = event.target.value
+      updatedOrderForm[formElementIdentifier] = updatedFormElement
+      this.setState({orderForm: updatedOrderForm})
+    }
+
     render() {
+
+      const formElementsArray = [];
+      for(let key in this.state.orderForm){
+        formElementsArray.push(
+          {
+            id: key,
+            config: this.state.orderForm[key]
+          }
+        )
+      }
       
       let form = (
-              <form>
-                <input className = {classes.Input} type='text' name='name' placeholder='Your name' />
-                <input className = {classes.Input} type='email' name='email' placeholder='Your email' />
-                <input className = {classes.Input} type='text' name='street' placeholder='Street' />
-                <input className = {classes.Input} type='text' name='postalCode' placeholder='Postal code' />
-                <Button btnType='Success' clicked = {this.orderHandler}>Order</Button>
+              <form onSubmit = {this.orderHandler}>
+                {formElementsArray.map(formElement => (
+                  <Input
+                    key = {formElement.id} 
+                    elementType = {formElement.config.elementType}
+                    elementConfig = {formElement.config.elementConfig}
+                    value = {formElement.config.value}
+                    changed = {(event) => this.inputChangedHandler(event, formElement.id)}  />
+                ))}
+                <Button btnType='Success'>Order</Button>
             </form>
       );
 
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
 
 class Orders extends React.Component{
+    
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount(){
+        axios.get('https://react-burger-builder-3c2a3.firebaseio.com/orders.json')
+            .then(res => {
+                const fetchedOrders = []
+                for(let key in res.data){
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                }
+                this.setState({orders: fetchedOrders, loading: false})
+            })
+    }
     render(){
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order key = {order.id }
+                        ingredients = {order.ingredients}
+                        price= {order.price} />
+                ))}
             </div>
         )
     }
